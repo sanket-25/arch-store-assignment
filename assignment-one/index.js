89f947a972cbba77c428f52f9bbd804a6f01ca6d
Sanket Dhuri <contact.sanketdhuri@gmail.com>
2023-11-30 23:28:54 +0530

    Add files via upload

--- assignment-one/index.js
+++ assignment-one/index.js
@@ -1,24 +1,112 @@
+const fs = require('fs');
 const https = require('https');
 
 const url = 'https://catfact.ninja/breeds';
 
+function logResponseToFile(data) {
+  fs.writeFile('response.txt', data, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('Response saved to response.txt');
+      getNumberOfPages();
+    }
+  });
+}
+
+function getNumberOfPages() {
+  https.get(url, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      try {
+        const jsonData = JSON.parse(data);
+        const totalPages = jsonData.last_page;
+
+        console.log('Total pages available:', totalPages);
+        getAllData(totalPages);
+      } catch (error) {
+        console.error('Error parsing JSON:', error.message);
+      }
+    });
+  }).on('error', (error) => {
+    console.error('Error fetching data:', error.message);
+  });
+}
+
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      let data = '';
+
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        try {
+          const jsonData = JSON.parse(data);
+          resolve(jsonData);
+        } catch (error) {
+          reject(new Error('Error parsing JSON:', error.message));
+        }
+      });
+    }).on('error', (error) => {
+      reject(new Error('Error fetching data:', error.message));
+    });
+  });
+}
+
+async function getAllData(totalPages) {
+  let allData = [];
+
+  try {
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+      const currentPageData = await fetchData(`${url}?page=${currentPage}`);
+      allData = allData.concat(currentPageData.data);
+    }
+
+    console.log('All data fetched:', allData);
+    const breedsByCountry = groupBreedsByCountry(allData);
+    console.log(JSON.stringify(breedsByCountry, null, 2));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function groupBreedsByCountry(allData) {
+  const breedsByCountry = {};
+
+  allData.forEach((breed) => {
+    const country = breed.country;
+    if (!breedsByCountry[country]) {
+      breedsByCountry[country] = [];
+    }
+    breedsByCountry[country].push({
+      breed: breed.breed,
+      origin: breed.origin,
+      coat: breed.coat,
+      pattern: breed.pattern
+    });
+  });
+
+  console.log('Breeds grouped by country:', breedsByCountry);
+  return breedsByCountry;
+}
+
 https.get(url, (response) => {
   let data = '';
 
-  // A chunk of data has been received.
   response.on('data', (chunk) => {
     data += chunk;
   });
 
-  // The whole response has been received.
   response.on('end', () => {
-    try {
-      const jsonData = JSON.parse(data);
-      console.log(jsonData);
-      // Handle the received data as needed here
-    } catch (error) {
-      console.error('Error parsing JSON:', error.message);
-    }
+    logResponseToFile(data);
   });
 }).on('error', (error) => {
   console.error('Error fetching data:', error.message);
--- assignment-two/index.js
+++ assignment-two/index.js
@@ -1,49 +1,66 @@
 const http = require('http');
+const PORT = 3000;
 
-const port = 3000;
 const server = http.createServer((req, res) => {
-  if (req.method === "POST" && req.url === "/") {
-    let data = "";
+  if (req.method === 'POST' && req.url === '/') {
+    let data = '';
 
-    req.on("data", (chunk) => {
+    req.on('data', (chunk) => {
       data += chunk;
     });
 
-    req.on("end", () => {
+    req.on('end', () => {
       try {
         const payload = JSON.parse(data);
-        if (!payload || !payload.str) {
-          res.writeHead(400, { "Content-Type": "application/json" });
-          res.end(JSON.stringify({ "error": "Invalid payload" }));
-        } else {
-          const receivedString = payload.str;
-          
-          console.log("Received String:", receivedString);
+        if (payload && payload.str) {
+          const wordCount = (payload.str.match(/\S+/g) || []).length;
 
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify({ "message": "Received the string successfully", "receivedString": receivedString }));
+          if (wordCount >= 8) {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'At least 8 words found' }));
+          } else {
+            res.writeHead(406, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ error: 'Not enough words (less than 8)' }));
+          }
+        } else {
+          res.writeHead(400, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Invalid payload format' }));
         }
       } catch (error) {
-        res.writeHead(500, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ "error": "Internal server error" }));
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Internal server error' }));
       }
     });
   } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ "error": "Route not found" }));
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Route not found' }));
   }
 });
 
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
 
 
-// use the POST method
-/*
 
+// Method POST in the body
+/*
 {
-    "str": "some words"
+    "str": "one two three four five"
+}
+output ->
+status: 406 not acceptable
+{
+  "error": "Not enough words (less than 8)"
 }
 
+
+{
+    "str": "one two three four five six seven eight nine ten"
+}
+output -> 
+status: 200 OK
+{
+  "message": "At least 8 words found"
+}
 */
